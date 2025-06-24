fae3bc79bb0a649686e0052c718ba25731622420
doneemoos <doneemooscristian@gmail.com>
2025-06-24 14:33:15 +0300

    Push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 import "./Navbar.css";
-import logo from "./img/UrbanAi_logo_transparent.png";
+import Logo from "./img/UrbanAi_logo_transparent.png";
 
 function Navbar() {
   const { isAuthenticated, logout } = useAuth();
@@ -14,50 +14,34 @@ function Navbar() {
   };
 
   return (
-    <nav className="borderNav">
-      <div className="navStyle">
-        <div>
-          <img
-            src={logo}
-            alt="UrbanAI Logo"
-            height="100rem"/>
-          <ul>
-            <li>
-              <Link to="/">Acasă</Link>
-            </li>
-            {isAuthenticated && (
-              <>
-                <li>
-                  <Link to="/dashboard">Dashboard</Link>
-                </li>
-                <li>
-                  <Link to="/map">Hartă</Link>
-                </li>
-                <li>
-                  <Link to="/leaderboard">Leaderboard</Link>
-                </li>
-                <li>
-                  <Link to="/report">Raportează</Link>
-                </li>
-                <li>
-                  <button onClick={handleLogout}>Log out</button>
-                </li>
-              </>
-            )}
-            {!isAuthenticated && (
-              <>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/signup">Signup</Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div className="navStyle">
+      <ul className="split1">
+       <a href="/home"><img className="logo" src={Logo}></img></a> 
+      </ul>
+      <nav className="navbar">
+        <ul>
+          {isAuthenticated ? (
+            <>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Log out</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign up</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
